7b69447611962529b59094cab71ba842fdb09a33
Abhijay Patne <abhijaypatne@gmail.com>
2016-05-09 01:45:00 -0500

    Array operations

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -5,28 +5,51 @@ object HelloWorld {
     println("Hello world"); 
     
     //stringOperations();
-    var returnVal = loopOperations(5, "string" );
-    println("Area of circle: " + circleArea());
+    var returnVal = arrayOperations(5, "string" );
+/*    println("Area of circle: " + circleArea());
     println("Area of circle with radius 5: " + circleArea(5));
-   
-  }
+    println("Area of rectangle: " + rectangleArea(width = 10, length=14)); 
+    println("multiplier: " + multiplier(10));
+    println("multiplie1: " + multiplier1(24, 10));
+*/  }
+  
+  // closure
+  val factor = 3.2f;
+  val multiplier = (i:Int) => i * factor;
+  var multiplier1 = (i:Int, j:Float) => i * j * factor;
   
   // try default function parameters 
   def circleArea( radius:Float = 1) : Float = {
     var pi : Float = 3.14f;
     return pi*radius*radius;
   }
+  
+  // named and default parameters
+  def rectangleArea (length:Float=1, width:Float=2) : Float = {
+    return length*width;
+  }
+
   //loop operations
-  def loopOperations( arg1:Int, arg2:String) : Int = {
-    var myArray = Array(4,5,6,7,23,45,67);
+  def arrayOperations( arg1:Int, arg2:String) : Int = {
+    var myArray = Array(4,5,6,7,230,23,23, 5,6,45,46,0, 46, 69, 67);
     for ( x <- myArray ){
       print(x + " ");
     }
     println();
-    /*for (i =0; i<myArray.length; i++){
-
+    //count operation
+    println("number of odd elements: " + myArray.count { x => (x%2!=0) });
+    //exists
+    println("does array contains number divisible by 5: " + myArray.exists {( x => (x%5==0) )});
+    // filter
+    println("numbers divisible by 23: " + myArray.filter { x => (x%23==0) }.toString());
+    println("total numbers divisible by 23: " + myArray.filter { x => (x%23==0) }.length);
+    println("unique numbers divisible by 23: " + myArray.filter { x => (x%23==0) }.distinct.length);
+    //distinct
+    var distinctArray = myArray.distinct;
+    for ( x <- distinctArray ){
+      print(x + " ");
     }
- */ 
+    println();
     return myArray.length;
     }
   
